5b30b7bfaa0465030bd650c7ea5df70e8f7af715
JPLizcano <jplizcano69@gmail.com>
2024-02-29 20:23:22 -0500

    validaciones de usuarios pero falta arreglar el put en usuario, correo y celular para que se pueda dejar el actual y no sea obligatorio cambiarlo

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -20,6 +20,7 @@ const usuariosPost = async (req, res = response)=>{
 
     const valEm = /^[a-zA-Z0-9]+@[a-zA-Z]{4,8}\.[a-zA-Z]{2,4}$/;
     const valPa = /^[a-zA-Z0-9]{8,15}$/;
+    const valCel = /^[0-9]{10}$/;
     const existeUsuario = await Usuarios.findOne( {where: { Usuario: body.Usuario} });
     const existeCorreo = await Usuarios.findOne( {where: { Correo: body.Correo} });
     const existeCelular = await Usuarios.findOne( {where: { Celular: body.Celular} });
@@ -48,13 +49,17 @@ const usuariosPost = async (req, res = response)=>{
         return res.status(400).json({
             mensaje: "El apellido no puede estar vacío!",
         });
-    }  else if (!body.Celular) {
+    } else if (!body.Celular) {
         return res.status(400).json({
             mensaje: "El número de celular no puede estar vacío!",
         });
     } else if (existeCelular) {
         return res.status(400).json({
             mensaje: "Ese número de celular ya está en uso!",
+        });
+    } else if (body.Celular != body.Celular.match(valCel)) {
+        return res.status(400).json({
+            mensaje: "El número de celular es inválido!",
         });
     } else if (!body.Correo) {
         return res.status(400).json({
@@ -63,10 +68,6 @@ const usuariosPost = async (req, res = response)=>{
     } else if (existeCorreo) {
         return res.status(400).json({
             mensaje: "Ese correo ya está en uso!",
-        });
-    } else if (body.Correo != body.Correo.match(valEm) && body.Pass != body.Pass.match(valPa)) {
-        return res.status(400).json({
-            mensaje: "El correo y la contraseña son inválidos!",
         });
     } else if (body.Correo != body.Correo.match(valEm)) {
         return res.status(400).json({
@@ -94,34 +95,98 @@ const usuariosPost = async (req, res = response)=>{
     })
 }
 
-const usuariosPut = async(req, res = response)=>{
-
+const usuariosPut = async (req, res = response)=>{
     const {IdUsuario, IdRol, Usuario,  Nombre, Apellidos, Correo, Celular, Pass } = req.body
     let mensaje = 'Modificación exitosa'
-    try{
 
-        const find = await Usuarios.findByPk(IdUsuario);
-        console.log(find);
-        find != null ? 
-        await Usuarios.update(
-            {
-                IdRol: IdRol,
-                Usuario: Usuario,
-                Nombre: Nombre,
-                Apellidos: Apellidos,
-                Correo: Correo,
-                Celular: Celular,
-                Pass: Pass
-            },
-            {
-                where: {
-                    IdUsuario: IdUsuario
+    const valEm = /^[a-zA-Z0-9]+@[a-zA-Z]{4,8}\.[a-zA-Z]{2,4}$/;
+    const valPa = /^[a-zA-Z0-9]{8,15}$/;
+    const valCel = /^[0-9]{10}$/;
+    const existeUsuario = await Usuarios.findOne( {where: { Usuario: Usuario} });
+    const existeCorreo = await Usuarios.findOne( {where: { Correo: Correo} });
+    const existeCelular = await Usuarios.findOne( {where: { Celular: Celular} });
+    
+    if (!IdRol && !Usuario && !Nombre && !Apellidos && !Correo && !Celular && !Pass) {
+        return res.status(400).json({
+            mensaje: "Debe llenar todos los campos!",
+        });
+    } else if (!IdRol) {
+        return res.status(400).json({
+            mensaje: "Debe seleccionar un rol!",
+        });
+    } else if (!Usuario) {
+        return res.status(400).json({
+            mensaje: "El usuario no puede estar vacío!",
+        });
+    } else if (existeUsuario) {
+        return res.status(400).json({
+            mensaje: "Ese usuario ya existe!",
+        });
+    } else if (!Nombre) {
+        return res.status(400).json({
+            mensaje: "El nombre no puede estar vacío!",
+        });
+    } else if (!Apellidos) {
+        return res.status(400).json({
+            mensaje: "El apellido no puede estar vacío!",
+        });
+    } else if (!Celular) {
+        return res.status(400).json({
+            mensaje: "El número de celular no puede estar vacío!",
+        });
+    } else if (existeCelular) {
+        return res.status(400).json({
+            mensaje: "Ese número de celular ya está en uso!",
+        });
+    } else if (Celular != Celular.match(valCel)) {
+        return res.status(400).json({
+            mensaje: "El número de celular es inválido!",
+        });
+    } else if (!Correo) {
+        return res.status(400).json({
+            mensaje: "El correo no puede estar vacío!",
+        });
+    } else if (existeCorreo) {
+        return res.status(400).json({
+            mensaje: "Ese correo ya está en uso!",
+        });
+    } else if (Correo != Correo.match(valEm)) {
+        return res.status(400).json({
+            mensaje: "El correo es inválido!",
+        });
+    } else if (!Pass) {
+        return res.status(400).json({
+            mensaje: "La contraseña no puede estar vacía!",
+        });
+    } else if (Pass != Pass.match(valPa)) {
+        return res.status(400).json({
+            mensaje: "La contraseña es inválida!",
+        });
+    } else {
+        try{
+            const find = await Usuarios.findByPk(IdUsuario);
+            console.log(find);
+            find != null ? 
+            await Usuarios.update(
+                {
+                    IdRol: IdRol,
+                    Usuario: Usuario,
+                    Nombre: Nombre,
+                    Apellidos: Apellidos,
+                    Correo: Correo,
+                    Celular: Celular,
+                    Pass: Pass
+                },
+                {
+                    where: {
+                        IdUsuario: IdUsuario
+                    }
                 }
-            }
-        ) : mensaje = 'No existe el usuario para ser modificado...'
-    }
-    catch(error){
-        mensaje = 'Se presentaron problemas al modificar el usuario...'
+            ) : mensaje = 'No existe el usuario para ser modificado...'
+        }
+        catch(error){
+            mensaje = 'Se presentaron problemas al modificar el usuario...'
+        }
     }
     res.json({
         msg: mensaje
