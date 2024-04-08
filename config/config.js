83de8bab76b02928fdb4b3a0e8c5ecd6196ccff7
JPLizcano <jplizcano69@gmail.com>
2024-04-08 15:12:43 -0500

    bdCloud

--- config/config.js
+++ config/config.js
@@ -1,13 +1,15 @@
 const { Sequelize }= require('sequelize');
 const db = new Sequelize({
     dialect: 'mysql',
-    // host: '127.0.0.1',
-    host: '34.134.5.40',
-    // username: 'root',
-    username: 'lizcano',
-    password: 'lizcano123',
+    host: '127.0.0.1',
+    // host: '34.134.5.40',
+    username: 'root',
+    // username: 'lizcano',
+    password: '',
+    // password: 'lizcano123',
     // password: 'admin', para el SENA
-    database: 'TWBS',
+    database: 'twbs',
+    // database: 'TWBS',
     logging: false,
     define: {
         timestamps: true
