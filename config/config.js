939c4c607315dea2dbed6b63573e1644172297e4
JPLizcano <jplizcano69@gmail.com>
2024-04-08 14:31:56 -0500

    update

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
 const { Sequelize }= require('sequelize');
 const db = new Sequelize({
     dialect: 'mysql',
-    host: '127.0.0.1',
+    // host: '127.0.0.1',
+    host: '34.134.5.40',
+    // username: 'root',
     username: 'root',
-    password: '',
+    password: 'admin123',
     // password: 'admin', para el SENA
     database: 'twbs',
     logging: false,
