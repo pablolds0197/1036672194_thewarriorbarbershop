1cfe23b6160df51383055126082c0a3acd36ef15
JPLizcano <jplizcano69@gmail.com>
2024-04-09 12:42:50 -0500

    arreglosProductos

--- models/gestionproductos.js
+++ models/gestionproductos.js
@@ -11,7 +11,7 @@ Gestionproducto.init({
     unique: true,
     type: DataTypes.INTEGER
   },
-  NombreDelInsumo: {
+  NombreProducto: {
     allowNull: false,
     type: DataTypes.STRING,
     unique: true
