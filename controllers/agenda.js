d53bde9502997f1a0552456479b07d35faae66de
JPLizcano <jplizcano69@gmail.com>
2024-04-15 12:56:12 -0500

    avance

--- controllers/agenda.js
+++ controllers/agenda.js
@@ -15,38 +15,21 @@ const agendaGet = async (req, res) => {
 const agendaPost = async (req, res = response) => {
   let mensaje = "Agenda registrada extosamente...";
   const body = req.body;
-  if (!body.IdEmpleado) {
-    return res.status(400).json({
-      mensaje: "Debe seleccionar un barbero!",
-    });
-  } else if (!body.IdServicio) {
-    return res.status(400).json({
-      mensaje: "El id de servicio no puede estar vacío",
-    });
-  } else if (!body.IdCliente) {
-    return res.status(400).json({
-      mensaje: "El id de cliente no puede estar vacío",
-    });
-  } else if (!body.FechaAgenda) {
-    return res.status(400).json({
-      mensaje: "Debe seleccionar una fecha!",
-    });
-  } else if (!body.HoraAgenda) {
-    return res.status(400).json({
-      mensaje: "Debe seleccionar una hora!",
-    });
-  } else if (!body.Valor) {
-    return res.status(400).json({
-      mensaje: "El valor no puede estar vacío!",
-    });
-  } else {
-    try {
-      const agenda = new Agenda(body);
-      agenda.save();
-    } catch (error) {
-      mensaje = error;
-      console.log(error);
+  try {
+    const agenda = new Agenda(body);
+    await agenda.save();
+    if (body.Estado && body.IdAgenda) {
+      setTimeout(async () => {
+        await Agenda.update(
+          { Estado: false },
+          { where: { IdAgenda: body.IdAgenda } }
+        );
+        console.log("Estado cambiado a terminado...");
+      }, 10000);
     }
+  } catch (error) {
+    mensaje = error;
+    console.log(error);
   }
   res.json({
     msg: mensaje,
@@ -65,64 +48,36 @@ const agendaPut = async (req, res = response) => {
     Valor,
   } = req.body;
   let mensaje = "Modificación exitosa";
-
-  if (!IdEmpleado) {
-    return res.status(400).json({
-      mensaje: "Debe seleccionar un barbero!",
-    });
-  } else if (!IdServicio) {
-    return res.status(400).json({
-      mensaje: "Debe seleccionar un servicio!",
-    });
-  } else if (!IdCliente) {
-    return res.status(400).json({
-      mensaje: "Debe seleccionar un cliente!",
-    });
-  } else if (!FechaAgenda) {
-    return res.status(400).json({
-      mensaje: "Debe seleccionar una fecha!",
-    });
-  } else if (!HoraAgenda) {
-    return res.status(400).json({
-      mensaje: "Debe seleccionar una hora!",
-    });
-  } else if (!Valor) {
-    return res.status(400).json({
-      mensaje: "Debe seleccionar una hora!",
-    });
-  } else {
-    try {
-      const find = await Agenda.findByPk(IdAgenda);
-      if (find != null) {
-        // Actualizar la agenda
-        await Agenda.update(
-          {
-            IdServicio: IdServicio,
-            IdEmpleado: IdEmpleado,
-            IdCliente: IdCliente,
-            Estado: Estado,
-            FechaAgenda: FechaAgenda,
-            HoraAgenda: HoraAgenda,
-            Valor: Valor,
+  try {
+    const find = await Agenda.findByPk(IdAgenda);
+    if (find != null) {
+      await Agenda.update(
+        {
+          IdServicio: IdServicio,
+          IdEmpleado: IdEmpleado,
+          IdCliente: IdCliente,
+          Estado: Estado,
+          FechaAgenda: FechaAgenda,
+          HoraAgenda: HoraAgenda,
+          Valor: Valor,
+        },
+        {
+          where: {
+            IdAgenda: IdAgenda,
           },
-          {
-            where: {
-              IdAgenda: IdAgenda,
-            },
-          }
-        );
-        if (Estado) {
-          setTimeout(() => {
-            Agenda.update({ Estado: false }, { where: { IdAgenda: IdAgenda } });
-            console.log("Estado cambiado a terminado...");
-          }, 60000);
         }
-      } else {
-        mensaje = "No existe la agenda para ser modificada...";
+      );
+      if (Estado) {
+        setTimeout(() => {
+          Agenda.update({ Estado: false }, { where: { IdAgenda: IdAgenda } });
+          console.log("Estado cambiado a terminado...");
+        }, 10000);
       }
-    } catch (error) {
-      mensaje = "Se presentaron problemas al modificar la agenda...";
+    } else {
+      mensaje = "No existe la agenda para ser modificada...";
     }
+  } catch (error) {
+    mensaje = "Se presentaron problemas al modificar la agenda...";
   }
   res.json({
     msg: mensaje,
--- routes/agenda.js
+++ routes/agenda.js
@@ -1,19 +1,49 @@
-const { Router} = require('express');
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validarCampos");
 
-const { agendaGet,
-        agendaPut,
-        agendaPost,
-        agendaDelete } = require('../controllers/agenda');
+const {
+  agendaGet,
+  agendaPut,
+  agendaPost,
+  agendaDelete,
+} = require("../controllers/agenda");
 
 const router = Router();
 
-router.get('/', agendaGet);
+router.get("/", agendaGet);
 
-router.put('/', agendaPut);
+router.put(
+  "/",
+  [
+    check("IdAgenda", "El id de la agenda es obligatorio.").not().isEmpty(),
+    check("IdEmpleado", "El id del empleado es obligatorio.").not().isEmpty(),
+    check("IdCliente", "El id del cliente es obligatorio.").not().isEmpty(),
+    check("IdServicio", "El id del servicio es obligatorio.").not().isEmpty(),
+    check("Estado", "El estado es obligatorio.").not().isEmpty(),
+    check("FechaAgenda", "La fecha es obligatoria.").not().isEmpty(),
+    check("HoraAgenda", "La hora es obligatoria.").not().isEmpty(),
+    check("Valor", "El valor es obligatorio.").not().isEmpty(),
+    validarCampos,
+  ],
+  agendaPut
+);
 
-router.post('/', agendaPost );
+router.post(
+  "/",
+  [
+    check("IdEmpleado", "El id del empleado es obligatorio.").not().isEmpty(),
+    check("IdCliente", "El id del cliente es obligatorio.").not().isEmpty(),
+    check("IdServicio", "El id del servicio es obligatorio.").not().isEmpty(),
+    check("Estado", "El estado es obligatorio.").not().isEmpty(),
+    check("FechaAgenda", "La fecha es obligatoria.").not().isEmpty(),
+    check("HoraAgenda", "La hora es obligatoria.").not().isEmpty(),
+    check("Valor", "El valor es obligatorio.").not().isEmpty(),
+    validarCampos,
+  ],
+  agendaPost
+);
 
-router.delete('/', agendaDelete);
-
+router.delete("/", agendaDelete);
 
 module.exports = router;
